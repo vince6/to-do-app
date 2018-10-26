590ed05412773e7344fcf813d508065a7911c0a8
Vincent Rodriguez <vince@Vincents-MacBook-Pro.local>
2018-10-26 15:18:31 -0700

    checkpoint 6 complete

--- app.js
+++ app.js
@@ -1,50 +1,47 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+const toDos = [];
+const addToDoForm = document.getElementById('addToDoForm');
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) {return;}
+    
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+    newToDoText.value = '';
+
+    renderTheUI();
+  }
+
+  function renderTheUI(){
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      });
+
+  }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-
-    // get the text
-    let title = newToDoText.value;
-
-    // create a new li
-    let newLi = document.createElement('li');
-
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox";
-
-    // set the title
-    newLi.textContent = title;
-
-    // attach the checkbox to the li
-    newLi.appendChild(checkbox);
-
-    // attach the li to the ul
-    toDoList.appendChild(newLi);
-
-    //create delete button
-    let minusBtn = document.createElement('button');
-
-    //set the delete button
-    minusBtn.innerHTML = "Delete";
-
-    //attach delete button to li
-    newLi.appendChild(minusBtn);
-
-    minusBtn.addEventListener('click', function() {
-      newLi.parentNode.removeChild(newLi);
-})
-
-    // empty the input
-    newToDoText.value = '';
-
+    createNewToDo();
   });
+
+  renderTheUI();
 }
+
 
 window.onload = function() {
    onReady();
